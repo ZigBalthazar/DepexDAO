e1a230362af0bcf9cc7daf71dd4a28a545b6e829
pouya <pouya@radhoosh.ir>
2023-01-10 19:28:11 +0330

    init ERC20Vote from Oz

--- contracts/DepexToken.sol
+++ contracts/DepexToken.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-contract DepexToken is ERC20{
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+contract DepexToken is ERC20Votes{
 
-    uint256 constant MAXSUPPLY = 1000000000000000000000000;
+    uint256 constant MAXSUPPLY = 1_000_000_000_000_000_000_000_000;
 
-    constructor() ERC20("DEPEX","DPX"){
+    constructor() ERC20("DEPEX","DPX") ERC20Permit("DEPEX"){
         _mint(msg.sender, MAXSUPPLY);
     }
 
